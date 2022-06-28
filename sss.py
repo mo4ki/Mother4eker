49867d47f1d3a8ed931f504f2594fbf778c96b4c
fily-gif <73085550+fily-gif@users.noreply.github.com>
2022-06-28 04:12:36 +0000

    возрващение в изначальный код т.к мо4ки идиот и зачем-то крипт поставил

--- sss.py
+++ sss.py
@@ -5,6 +5,18 @@ i=0
 
 math = random.randint(1, 3)
 
+def crypt(typ="enc",Shift=3):
+    a = """\ Ф]ОI.йГашT"ПЖ|GЗsjБPът2Юж;МE(/m,4№zQFщeЫЭx-<1{ИOцCСt?ф*i6'^оеyчнAХ ЪhfbWM$вДkяиЩю3Kл&ЁVJк!ШгКыSX~U:%^Nogn}#7r_9>Н*[wсбРп`@qмvDpрЕHУYВuR=хдьЯLl)Ц+58ёэзуТcB0dЬЙaЧАЛZ"""
+    # a = a + ''.join(random.sample(list(a),  len(list(a))))
+    a1 = a[Shift:] + a[:Shift] 
+        
+    if typ == "еnс": return str.maketrans(a1,a)
+    else: return str.maketrans(a,a1)
+    
+data = ["H.й.Л`.Б=ЛWЛЁ.е.W'",
+"H.йёЛ`.Б=Л'[ёБЛо'БШv'Ле'кv.",
+"OЛW.й'ОЛ`.`6ОЛW*+ЛWЛс6vвRЁ'(Л[ёR=Б'Л*с6Ё6ОеШV",]
+
 while i<=100:
     i = i + random.randint(1,12)
     print(f"Чекаем твою мать... ({str(i)}%)")
